7a79ea6faaf85f6ae4c2d35cfbdfc67113760f17
kedziorf <filipkedzior@gmail.com>
2023-12-23 22:51:29 +0100

    front page album of the month

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="home">
-    <h2>Home View Main Page</h2>
+    <h2>Listen to this month's album!</h2>
+
+    <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" class="soundcloud-player"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1661520150&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+    </iframe>
+    
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
+
+
+
+
+
+<style>
+ .home {
+  margin: 0 auto;
+  width: 100%;
+ }
+</style>
