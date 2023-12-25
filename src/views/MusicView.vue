27b93f93fe4b631da906c9e75068f4cf98a82471
kedziorf <filipkedzior@gmail.com>
2023-12-25 17:33:04 +0100

    json db works and can fetch from it but dunno how to put in :((

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -2,6 +2,25 @@
   <div class="musicLibrary">
     <h2>Music View Library Page</h2>
   </div>
+
+  <div v-for="file in audio_files" :key="file.id">
+      <h2>{{ file.name +" "+ file.author }}</h2>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+  export default{
+    data() {
+      return{
+        audio_files: []
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/audio_files')
+        .then(res => res.json())
+        .then(data => this.audio_files = data)
+        .catch(err => console.log(err.message))
+    }
+  }
+  
+</script>
