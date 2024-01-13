96ca6d5ee30417cb0653b65f396fab6e8e1ca3c9
JanNautalis <filipkedzior@gmail.com>
2024-01-13 14:24:14 +0100

    Edit form added to music with readonly inputs

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -2,36 +2,33 @@
   <div>
     <h1>Music Library</h1>
 
-    <button @click="togglePage">
-      {{ currentPage === 'library' ? 'Add new audio' : 'See the library' }}
-    </button>
+    <button @click="togglePage">{{ currentPage === 'library' ? 'Add Song' : 'Back to Library' }}</button>
 
     <div class="library-page">
       <div class="musicLibrary" v-if="currentPage === 'library'">
-        <div v-for="file in audio_files" :key="file.id">
-          <h2>{{ file.name + ' ' + file.author }}</h2>
+        <div v-for="file in audio_files" :key="file.id" @click="displaySongDetails(file)">
+          <h2>{{ file.name }}</h2>
         </div>
       </div>
 
       <div class="addToLibrary" v-if="currentPage === 'add'">
+        <h2>{{ selectedSong ? 'Edit Song' : 'Add Song' }}</h2>
 
-        <h2>Add Song</h2>
-
-        <form @submit.prevent="submitAudioForm">
+        <form @submit.prevent="selectedSong ? updateAudioForm() : submitAudioForm()">
           <label for="name">Name: </label>
-          <input type="text" v-model="formData.name" required>
+          <input type="text" v-bind:readonly="selectedSong" v-model="formData.name" required>
 
           <label for="author">Author:</label>
-          <input type="text" v-model="formData.author" required>
+          <input type="text" v-bind:readonly="selectedSong" v-model="formData.author" required>
 
           <label for="date">Date:</label>
-          <input type="date" v-model="formData.date" required>
+          <input type="date" v-bind:readonly="selectedSong" v-model="formData.date" required>
 
           <label for="genre">Genre:</label>
-          <input type="text" v-model="formData.genre" required>
+          <input type="text" v-bind:readonly="selectedSong" v-model="formData.genre" required>
 
           <label for="stars">Stars:</label>
-          <input type="number" v-model="formData.stars" min="0" max="10" required>
+          <input type="number"  v-model="formData.stars" min="0" max="10" required>
 
           <div class="inline-checkbox">
             <input type="checkbox" v-model="formData.favourite">
@@ -39,12 +36,12 @@
           </div>
 
           <label for="url">URL:</label>
-          <input type="url" v-model="formData.url" required>
+          <input type="url" v-bind:readonly="selectedSong" v-model="formData.url" required>
 
           <label for="color">Color:</label>
           <input type="color" v-model="formData.color" required>
 
-          <button type="submit">Submit</button>
+          <button type="submit">{{ selectedSong ? 'Update' : 'Submit' }}</button>
         </form>
       </div>
     </div>
@@ -68,7 +65,8 @@ export default {
         url: '',
         color: '#000000'
       },
-      currentPage: 'library'
+      currentPage: 'library',
+      selectedSong: null
     };
   },
   mounted() {
@@ -89,6 +87,21 @@ export default {
         })
         .catch(err => console.log(err));
 
+      this.resetFormData();
+      this.togglePage();
+      this.fetchAudioFiles();
+    },
+    updateAudioForm() {
+      axios.put(`http://localhost:3000/audio_files/${this.selectedSong.id}`, this.formData)
+        .then(() => {
+          this.fetchAudioFiles();
+        })
+        .catch(err => console.log(err));
+
+      this.selectedSong = null;
+      this.resetFormData();
+    },
+    resetFormData() {
       this.formData = {
         name: '',
         author: '',
@@ -99,13 +112,31 @@ export default {
         url: '',
         color: '#000000'
       };
+    },
+    displaySongDetails(song) {
+      this.selectedSong = song;
+      this.currentPage = 'add';
 
-      this.togglePage(); // Fixed: Call togglePage method
-      this.fetchAudioFiles();
+      this.formData.name = song.name;
+      this.formData.author = song.author;
+      this.formData.date = song.date;
+      this.formData.genre = song.genre;
+      this.formData.stars = song.stars;
+      this.formData.favourite = song.favourite;
+      this.formData.url = song.url;
+      this.formData.color = song.color;
     },
     togglePage() {
-      this.currentPage = this.currentPage === 'library' ? 'add' : 'library';
+    if (this.currentPage === 'library') {
+      // Przenosimy się do strony 'add', zresetuj selectedSong i dane formularza
+      this.currentPage = 'add';
+      this.selectedSong = null;
+      this.resetFormData();
+    } else {
+      // Przenosimy się z powrotem do strony 'library'
+      this.currentPage = 'library';
     }
+  }
   }
 };
 </script>
